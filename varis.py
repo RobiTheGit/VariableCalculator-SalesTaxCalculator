1019434edeacf0b4d7b9a393d3a7a590e2fda2f7
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2022-11-18 10:53:10 -0700

    Add files via upload

--- varis.py
+++ varis.py
@@ -1,6 +1,6 @@
 tax = .0625 #6 cents per dollar ID sales tax, also the program adds 1 to it so it simplifies the math
 message = 'Tax is .0625 per dollar in Idaho'
-mesage2 = 'price for this only, not added for total'
+mesage2 = 'price for this only with sales tax applied, not added for total'
 thinglist = ['Diet Dr Thunder', 'Painting', 'Porkchop', 'Laptop', 'Iphone', 'Diet Sunkist']
 costlist = [1.18, 2.96, 4.36, 530.00, 1080.00, 1.48]
 logo = '''
